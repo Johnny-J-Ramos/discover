c051c78da1ce57cfabd5e44a50b1d0765fce8461
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2021-11-22 21:53:44 -0300

    principais comando do Js

---
+++ js/array.js
@@ -0,0 +1,12 @@
+/* Array (vetores)
+   - Uma lista
+   - Agrupamentos de dados
+   
+   const name = [
+      "name":'Johnny',
+      "age": 27,
+      "profession": "Support"
+   ]
+*/
+
+console.log(["Johnny", 27, "Support"])
---
+++ js/object.js
@@ -0,0 +1,17 @@
+/* Object
+   - Objeto
+   - Propriedades / Atributos
+   - Funcionalidades / Métodos
+   
+   { propriedade: "valor" }
+*/
+
+
+console.log({ 
+   name: "Johnny",
+   age: 27,
+   andar: function() {
+      console.log('andar')
+   }
+})
+
