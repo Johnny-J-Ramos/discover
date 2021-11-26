70320a5d4e2cbba0255d9b4046af07c4c688c483
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2021-11-25 23:24:56 -0300

    iniciando com as functions

---
+++ js/functions/function1.js
@@ -0,0 +1,30 @@
+// CRIANDO FUNÇÕES
+
+// Paramentros dentro da uma função
+// const sum = function(Number1, Number2) {
+//    total = Number1 + Number2;
+//    return total;
+// }
+
+
+// let number1 = 32;
+// let number2 = 3232;
+
+// console.log(`O número 1 é ${number1}`);
+// console.log(`O número 2 é ${number2}`);
+// console.log(`a soma é ${sum(number1, number2)}`);
+
+
+// DateNas(4,5) //é passado argumento dentro
+
+const sum = function(Number1, Number2){
+   const total = Number1 + Number2
+   return total
+}
+
+let number1 = 342
+let number2 = 4232
+
+console.log(`O número 1 é ${number1}`)
+console.log(`O número 2 é ${number2}`)
+console.log(`a soma é ${sum(number1, number2)}`)
