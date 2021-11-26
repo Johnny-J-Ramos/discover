cbe336fbf3b49afc1a167341752ea7ad88d6e64f
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2021-11-26 00:01:18 -0300

    executando as function

--- js/functions/function1.js
+++ js/functions/function1.js
@@ -17,14 +17,40 @@
 
 // DateNas(4,5) //é passado argumento dentro
 
-const sum = function(Number1, Number2){
-   const total = Number1 + Number2
-   return total
+
+
+
+// const sum = function(number1, number2){
+//   var total = number1 + number2
+//    return total
+// }
+
+// let number1 = 342
+// let number2 = 4232
+
+
+// console.log(`O número 1 é ${number1}`)
+// console.log(`O número 2 é ${number2}`)
+// console.log(`a soma é ${sum(number1, number2)}`)
+
+// Outro Exemplo
+
+// function fazerSuco(fruta1, fruta2) {
+//    let total = fruta1 + fruta2; //carametro
+//    return total;
+
+// }
+
+// let copo = fazerSuco('banana', 'maça'); // argumentos
+
+// console.log(copo);
+
+// Function SCOPE
+
+let subject = 'create video'
+
+function createThink(subject) {
+   return subject
 }
 
-let number1 = 342
-let number2 = 4232
-
-console.log(`O número 1 é ${number1}`)
-console.log(`O número 2 é ${number2}`)
-console.log(`a soma é ${sum(number1, number2)}`)
+console.log(subject)
