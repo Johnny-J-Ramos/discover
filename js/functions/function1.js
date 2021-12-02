6952f681899b117dcc923d3c5cebadd4eb25ba9f
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2021-12-01 23:47:25 -0300

    concluindo o módulo function

--- js/functions/function1.js
+++ js/functions/function1.js
@@ -41,6 +41,59 @@
 
 // ## 003 FUNCTION HOISTING - SOBREPOSIÇÃO
 
+// ## 004 ARROY FUNCTION = criando funções de modo moderno
+
+      // const sayMyName = () => {
+      // console.log('Johnny')
+      // }
+
+      // sayMyName()
 
 
+// ## 005 CALLBACK FUNCTION - Chamar de volta
 
+      // function sayMyName(name) {
+      //    console.log('antes de executar uma função callback')
+         
+      //    name() // callback será chamada de volta
+
+      //    console.log('depois de executar uma função callback')
+      // }
+
+      // sayMyName(
+      //    () => {
+      //       console.log('estou em uma callbeck')
+      //    }
+         
+      // )
+
+// ## 2º Exemplo de callback
+      // function greeting(name) {
+      //    alert('Hello'.concat(name)) 
+      // }
+
+      // function processYserUnput(callback) {
+      //    const name= prompt('Please enter your name.')
+      //    callback(name)
+      // }
+
+      // processYserUnput(greeting)
+
+
+// ## 006 FUNCTION() CONSTRUCTOR
+      // expressão new
+      // criar um nobo objeto
+      // this key word
+
+function Person(name) {
+   this.name = name;
+   this.walk = function() {
+      return this.name + " esta andando "
+   }
+}
+
+const johnny = new Person("johnny")
+const jefferson = new Person("Jefferson") // inslanciar um novo objeto
+
+console.log(johnny.walk())
+console.log(jefferson.walk())
