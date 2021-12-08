2ea2acd937ba6be20c5d5f510e51c6d4fca31d93
Johnny Jefferson <johnnyjeffersonjr@hotmail.com>
2021-12-08 13:12:30 -0300

    iniciando com os operadores

---
+++ js/expressoesO/expressoesOperacionais.js
@@ -0,0 +1,18 @@
+// Expressões Operacionais 
+
+/* ##01 New 
+    left-gand-side expression
+    criar um novo objeto
+*/
+
+let name = new String('Johnny')
+name.surName = 'Jefferson'
+let age = new Number(27)
+
+console.log(name,age)
+
+let date = new Date(94, 19);
+
+
+console.log(date)
+
